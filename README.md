e375cd84ff97bbf393a3ddd052151d244127357e
jaysun <wx_97b8e53e64034716b197bc1623aa8c53@git.code.tencent.com>
2019-11-27 16:17:31 +0800

    change

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # laravel-fe-render
 
-使用vue+iview来写blade模板
 
+## 介绍
 
+用于配合base-fe项目渲染控制器的视图页面
+
+如果需要进行`src/assets/dist/js/app.bundle.js`修改，请参考[https://github.com/sunshinev/base-fe](https://github.com/sunshinev/base-fe)，可重新编译该项目。
--- src/views/layouts/index.blade.php
+++ src/views/layouts/index.blade.php
@@ -5,16 +5,14 @@
     <meta content="IE=edge" http-equiv=X-UA-Compatible>
     <meta content="width=device-width,initial-scale=1" name=viewport>
     <title></title>
-    <link href=https://account-book-1252058004.cos.ap-guangzhou.myqcloud.com/fe/dist/css/chunk-vendors.css
-          rel=stylesheet>
-    <link href={{URL::asset('gii_assets/base_fe/css/chunk-vendors.css')}}
+    <link href={{URL::asset('gii_assets/base_fe/dist/css/chunk-vendors.css')}}
           rel=stylesheet>
 </head>
 <body>
 <noscript><strong>We're sorry but account-book-fe doesn't work properly without JavaScript enabled. Please enable it to
         continue.</strong></noscript>
 <div id=app></div>
-<script src={{URL::asset('gii_assets/base_fe/js/chunk-vendors.js')}}></script>
-<script src={{URL::asset('gii_assets/base_fe/js/app.bundle.js')}}></script>
+<script src={{URL::asset('gii_assets/base_fe/dist/js/chunk-vendors.js')}}></script>
+<script src={{URL::asset('gii_assets/base_fe/dist/js/app.bundle.js')}}></script>
 </body>
 </html>
